f0fa9c5b0500e1b939778edf884fe405c709c989
symphony407 <velozsymphony@gmail.com>
2025-09-04 22:56:19 -0500

    Added cipher and decipher file

---
+++ Decipher.java
@@ -0,0 +1,38 @@
+package Decipher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Decipher {
+    public static void main(String[] args) {
+        int m = (int)Math.pow(2,7); //sets m to 128
+        char[] cipher = new char[ m ]; //creates a char array named cipher of length 128
+        for(int i = 0; i < m; i++) { //loops from 0 to 127
+            cipher[i] = (char) mod (55 * (i - 3) , m );//55*i+91 //fills the cipher array with the appropriate characters
+            String plain = read("Decipher\\cipher.txt");
+             for (int j = 0; j < plain.length(); j++)
+                     System.out.print(cipher[plain.charAt(j)]);
+                }
+            }
+        
+            public static String read (String path) {
+                String ret = null;
+                try{
+                    ret = new String(Files.readAllBytes(Paths.get(path)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return ret;
+             }
+             public static int mod(int x, int n) {
+                return((x%n)+ n) %n;
+             }
+            
+            
+            
+            
+            }
+
+        
+    
